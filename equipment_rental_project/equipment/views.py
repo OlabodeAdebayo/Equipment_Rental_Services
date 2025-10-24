ff03296d53f210f2c32a500b33def8d6fbe1caee
Olabode <adesamson0404@gmail.com>
2025-10-24 03:52:43 +0100

    Add views for equipment listing and detail management

--- equipment_rental_project/equipment/views.py
+++ equipment_rental_project/equipment/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Equipment
+from .serializers import EquipmentSerializer
 
-# Create your views here.
+class EquipmentListView(generics.ListCreateAPIView):
+    queryset = Equipment.objects.all()
+    serializer_class = EquipmentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class EquipmentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Equipment.objects.all()
+    serializer_class = EquipmentSerializer
+    permission_classes = [permissions.IsAuthenticated]
