759d3561268537e25b520800db96791b120bbe80
Olabode <adesamson0404@gmail.com>
2025-10-24 04:03:32 +0100

    Equipment model with fields and string representation

--- equipment_rental_project/equipment/models.py
+++ equipment_rental_project/equipment/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Equipment(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    available = models.BooleanField(default=True)
+    rented_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.SET_NULL
+    )
+
+    def __str__(self):
+        return self.name
+
