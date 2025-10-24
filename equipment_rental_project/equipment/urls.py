1f6a5890de8de78867d69c7896d852a6f61cb44d
Olabode <adesamson0404@gmail.com>
2025-10-24 04:35:13 +0100

    Replace EquipmentListView with EquipmentListCreateView

--- equipment_rental_project/equipment/urls.py
+++ equipment_rental_project/equipment/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import EquipmentListView, EquipmentDetailView
+from .views import EquipmentListCreateView, EquipmentDetailView
 
 urlpatterns = [
-    path('', EquipmentListView.as_view(), name='equipment-list'),
+    path('', EquipmentListCreateView.as_view(), name='equipment-list'),
     path('<int:pk>/', EquipmentDetailView.as_view(), name='equipment-detail'),
 ]
